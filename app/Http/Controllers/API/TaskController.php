35cf0f2ef047b15af0841185e51f6610f61c3d74
Joao Henrique <joaohenriquefsf@gmail.com>
2024-07-08 00:07:46 -0300

    creating controller, form request and routes

---
+++ app/Http/Controllers/API/TaskController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TaskFormRequest;
+use App\Services\Task\TaskServiceInterface;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    protected $taskService;
+
+    /**
+     * TaskController constructor.
+     *
+     * @param TaskServiceInterface $taskService
+     */
+    public function __construct(TaskServiceInterface $taskService)
+    {
+        $this->taskService = $taskService;
+    }
+
+    /**
+     * Display a listing of tasks.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        return response()->json($this->taskService->getAllTasks());
+    }
+
+    /**
+     * Store a newly created task.
+     *
+     * @param TaskFormRequest $request
+     * @return JsonResponse
+     */
+    public function store(TaskFormRequest $request): JsonResponse
+    {
+        $task = $this->taskService->createTask($request->validated());
+        return response()->json($task, 201);
+    }
+
+    /**
+     * Display the specified task.
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function show($id): JsonResponse
+    {
+        $task = $this->taskService->getTaskById($id);
+        return response()->json($task);
+    }
+
+    /**
+     * Update the specified task.
+     *
+     * @param TaskFormRequest $request
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function update(TaskFormRequest $request, $id): JsonResponse
+    {
+        $task = $this->taskService->updateTask($id, $request->validated());
+        return response()->json($task, 200);
+    }
+
+    /**
+     * Remove the specified task.
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function destroy($id): JsonResponse
+    {
+        $this->taskService->deleteTask($id);
+        return response()->json(null, 204);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\API\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api/tasks', [TaskController::class, 'index']);
+Route::post('/api/tasks', [TaskController::class, 'store']);
+Route::get('/api/tasks/{id}', [TaskController::class, 'show']);
+Route::put('/api/tasks/{id}', [TaskController::class, 'update']);
+Route::delete('/api/tasks/{id}', [TaskController::class, 'destroy']);
