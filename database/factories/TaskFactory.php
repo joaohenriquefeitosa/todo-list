f5679e7131ec81800a8c55814d2e42b9e4398f16
Joao Henrique <joaohenriquefsf@gmail.com>
2024-07-08 01:32:43 -0300

    adding seeders and fixing routes to api

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    protected $model = Task::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'due_date' => $this->faker->dateTimeBetween('now', '+1 year')->format('Y-m-d'),
+            'status' => $this->faker->randomElement(['pending', 'completed']),
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/api/tasks', [TaskController::class, 'index']);
-Route::post('/api/tasks', [TaskController::class, 'store']);
-Route::get('/api/tasks/{id}', [TaskController::class, 'show']);
-Route::put('/api/tasks/{id}', [TaskController::class, 'update']);
-Route::delete('/api/tasks/{id}', [TaskController::class, 'destroy']);
